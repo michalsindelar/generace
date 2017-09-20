9aac1391b5565ebf6a7aebd78984f4675020c6d1
Michal Sindelar <michalsindelar03@gmail.com>
2017-09-20 19:14:37 +0200

    NEW: whole site

--- react-ui/src/scenes/MainPage/index.js
+++ react-ui/src/scenes/MainPage/index.js
@@ -5,7 +5,6 @@ import { FullPage } from "react-full-page"
 
 import Appendix from "./components/Appendix"
 import Attendance from "./components/Attendance"
-import General from "./components/General"
 import Header from "../../components/Header"
 import Info from "./components/Info"
 import Intro from "./components/Intro"
@@ -14,9 +13,9 @@ const MainPage = () =>
   <div className="MainPage">
     <Header/>
     <FullPage style={{backgroundColor: "red"}}>
-      {/*<Intro />*/}
-      {/*<Attendance />*/}
-      {/*<Info />*/}
+      <Intro />
+      <Attendance />
+      <Info />
       <Appendix />
     </FullPage>
   </div>
