9d6557d51248b45c7a42e4c11ccd2599becdc0e1
Michal Sindelar <michalsindelar03@gmail.com>
2017-10-01 17:13:41 +0200

    dev: disable api server

--- server/index.js
+++ server/index.js
@@ -8,10 +8,10 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
 // Answer API requests.
-app.get('/api', function (req, res) {
-  res.set('Content-Type', 'application/json');
-  res.send('{"message":"Hello from the custom server!"}');
-});
+// app.get('/api', function (req, res) {
+//   res.set('Content-Type', 'application/json');
+//   res.send('{"message":"Hello from the custom server!"}');
+// });
 
 // All remaining requests return the React app, so it can handle routing.
 app.get('*', function(request, response) {
