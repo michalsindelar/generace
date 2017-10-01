5a18891492a8484bdd3f21109446f0aabb9d192f
Michal Sindelar <michalsindelar03@gmail.com>
2017-10-01 17:14:16 +0200

    new: correct demagog link

--- react-ui/src/services/consts.js
+++ react-ui/src/services/consts.js
@@ -5,7 +5,7 @@ export const INSTAGRAM_LINK = "https://www.instagram.com/generacesnazorem/"
 export const VOLEBNI_CALC_LINK = "https://volebnikalkulacka.cz/"
 export const MAPA_VOLEBNICH_LINK = "https://volby.tmapy.cz/"
 export const ONLINE_VOLEBNI_PRUKAZ_LINK = "https://volby.hlidacstatu.cz/"
-export const DEMAGOG = "https://demagog.cz"
+export const DEMAGOG = "http://demagog.cz"
 
 export const INFO_EMAIL = "info@generacesnazorem.cz"
 export const AUTHOR = "Jakub Ondráček"
