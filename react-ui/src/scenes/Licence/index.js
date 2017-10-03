02aa6ded54a044ca17d5262cc819feb10d288813
Michal Sindelar <michalsindelar03@gmail.com>
2017-10-03 21:25:04 +0200

    volicsky prukaz

---
+++ react-ui/src/scenes/Licence/index.js
@@ -0,0 +1,16 @@
+// @flow
+import React, { PureComponent } from "react"
+
+class Licence extends PureComponent {
+
+  componentWillMount() {
+    window.location.replace("/volicskyPrukaz/")
+  }
+
+  render() {
+    return <div></div>
+  }
+
+}
+
+export default Licence
