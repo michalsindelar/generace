257af06a33d03c5526c3488fc45cc561a4f746b8
Michal Sindelar <michalsindelar03@gmail.com>
2017-11-18 22:11:59 +0100

    NEW: Video responsiveness

--- react-ui/src/scenes/MainPage/components/Video.js
+++ react-ui/src/scenes/MainPage/components/Video.js
@@ -3,19 +3,30 @@ import React from "react"
 import FullscreenSlide from "../../../components/FullscreenSlide/index"
 import styled from 'styled-components';
 
+import { MEDIA_ONLY_QUERY } from "../../../services/styleTools"
+
 const IframeWrapper = styled.div`
   & > iframe {
-    position: fixed;
-    right: 0;
-    bottom: 0;
-    min-width: 100%;
-    min-height: 100%;
-    width: auto;
-    height: auto;
-    z-index: 0;
+    
+      position: fixed;
+      right: 0;
+      bottom: 0;
+      min-width: 100%;
+      min-height: 100%;
+      width: auto;
+      height: auto;
+      z-index: 0;  
+      border: 0;
+    
+    ${MEDIA_ONLY_QUERY.TABLET`
+      position: inherit;
+      width: 100vw;
+      height: calc(100vh - 20px);
+      margin-left: -20px;
+    `}
+  
   }
 `
-
 
 const Video = () =>
   <FullscreenSlide className="Attendance">
